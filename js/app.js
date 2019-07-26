150bac5edf3b39f4e65e08a5bbfde54bbf1878b7
Shervon St.Brice <shervonrs@gmail.com>
2019-07-26 14:29:54 -0400

    update

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 const Parallax = require('parallax-js');
-// let mtn_parallax = document.getElementsByClassName('js-parallax-mtn');
-// var parallaxInstance = new Parallax(mtn_parallax);
+let mtn_parallax = document.getElementsByClassName('js-parallax-mtn');
+var parallaxInstance = new Parallax(mtn_parallax);
 
-// export.app= app
-// $(document).ready(function (){
-//   $('#fullpage').fullpage({
-//     autoScrolling:true,
-//     scrollHorizontally: true
-//   });
-//   // methods
-//   $.fn.fullpage.setAllowScrolling(true);
-// });
+export.app= app
+$(document).ready(function (){
+  $('#fullpage').fullpage({
+    autoScrolling:true,
+    scrollHorizontally: true
+  });
+  // methods
+  $.fn.fullpage.setAllowScrolling(true);
+});
